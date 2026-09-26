acffec603b31d70f69e46c9162e0e01de1fde634
mrfhmd7 <mrf.hmd7@gmail.com>
2026-09-26 13:24:13 +0600

    Update profile dropdown animation to 0.5s delay and 1s duration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -65,7 +65,7 @@ export default {
         },
       },
       animation: {
-        dropdown: 'dropdown 0.35s cubic-bezier(0.22, 1, 0.36, 1) forwards',
+        dropdown: 'dropdown 1s cubic-bezier(0.22, 1, 0.36, 1) 0.5s both',
       },
     },
   },
